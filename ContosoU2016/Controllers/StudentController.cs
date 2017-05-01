363eca47926f6c7d876928ccd96d975b8e9552e4
Marc Williams <oti_instructor@hotmail.com>
2017-05-01 12:33:15 -0300

    Added Sorting, Filtering on Students

---
+++ ContosoU2016/Controllers/StudentController.cs
@@ -0,0 +1,230 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoU2016.Data;
+using ContosoU2016.Models;
+
+namespace ContosoU2016.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentController(SchoolContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Student
+        public async Task<IActionResult> Index(string sortOrder, 
+                                               string searchString, 
+                                               string currentFilter)
+        {
+            //sortOrder:  for Sorting
+            //searchString:  for Searching 
+            //currentFilter:  to keep current search 
+
+            //return View(await _context.Students.ToListAsync());
+            //mwilliams:  add paging, sorting and filtering functionality
+            var students = from s in _context.Students
+                           select s; //SELECT FirstName, LastName, Email, EnrollmentDate FROM Students
+            
+            //Part 1:  Sorting
+
+            //Default Sort LastName
+            ViewData["LNameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "lname_desc" : "";
+
+            //Other Sort Orders //Toggle between ascending and descending
+            ViewData["FNameSortParm"] = sortOrder == "fname" ? "fname_desc" : "fname";
+            ViewData["EmailSortParm"] = sortOrder == "email" ? "email_desc" : "email";
+            ViewData["DateSortParm"] = sortOrder == "date" ? "date_desc" : "date";
+
+
+            //Part 2: Filtering
+            if (searchString == null)
+            {
+                searchString = currentFilter;
+            }
+            
+
+            ViewData["CurrentFilter"] = searchString;
+
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                //User entered a search criteria -> search by last name or first name
+                students = students.Where(s => s.LastName.Contains(searchString) ||
+                                          s.FirstName.Contains(searchString));
+
+            }
+
+
+            //Apply the Sorting
+            switch (sortOrder)
+            {
+                case "lname_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+               //FirstName
+                case "fname":
+                    students = students.OrderBy(s => s.FirstName);
+                    break;
+                case "fname_desc":
+                    students = students.OrderByDescending(s => s.FirstName);
+                    break;
+                //Email
+                case "email":
+                    students = students.OrderBy(s => s.Email);
+                    break;
+                case "email_desc":
+                    students = students.OrderByDescending(s => s.Email);
+                    break;
+                //Date
+                case "date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default: //LastName ascending
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+
+            }
+
+            return View(await students.ToListAsync());
+
+
+        }
+
+
+        // GET: Student/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Student/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EnrollmentDate,ID,LastName,FirstName,Email")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Student/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.SingleOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EnrollmentDate,ID,LastName,FirstName,Email")] Student student)
+        {
+            if (id != student.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Student/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.ID == id);
+        }
+    }
+}
--- ContosoU2016/Models/Person.cs
+++ ContosoU2016/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace ContosoU2016.Models
     {
         //mwilliams:  Create the data models 
         public int ID { get; set; }
-        public string LastName { get; set; }
+          public string LastName { get; set; }
         public string FirstName { get; set; }
         public string Email { get; set; }
 
