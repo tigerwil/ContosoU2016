a4952b52f59aeb29bab4bc1dd3c48c32731ac5a1
Marc Williams <oti_instructor@hotmail.com>
2017-05-18 12:29:28 -0300

    Ajax Faculty search

--- ContosoU2016/Controllers/HomeController.cs
+++ ContosoU2016/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ContosoU2016.Models;
+using ContosoU2016.Data;
 
 namespace ContosoU2016.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -26,6 +35,21 @@ namespace ContosoU2016.Controllers
 
             return View();
         }
+
+        private List<Instructor> GetFaculty(string searchString)
+        {
+            return _context.Instructors
+                .Where(i => i.LastName.Contains(searchString) ||
+                  i.FirstName.Contains(searchString)).ToList();
+        }
+
+        public ActionResult FacultySearch(string q)
+        {
+            //call getfaculty
+            var faculty = GetFaculty(q);
+            return PartialView("_FacultySearch", faculty);
+        }
+
 
         public IActionResult Error()
         {
